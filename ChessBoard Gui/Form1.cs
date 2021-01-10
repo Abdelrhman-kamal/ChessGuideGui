f9e2d203faa5d98278f44eb672a2bc7f59c242a5
Abdelrhman-Kamal <abdulrmankamal@gmail.com>
2021-01-10 17:53:05 +0200

    Uploading ChessGui Project

---
+++ ChessBoard Gui/Form1.cs
@@ -0,0 +1,107 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoard_Gui
+{
+    public partial class Form1 : Form
+    {
+
+        static Board myBoard = new Board(8);
+
+        public Button[,] btnGrid = new Button[myBoard.Size, myBoard.Size];
+        public Form1()
+        {
+            InitializeComponent();
+            populateGrid();
+        }
+
+        private void populateGrid()
+        {
+            int buttonSize = Panel1.Width / myBoard.Size;
+
+
+            Panel1.Height = Panel1.Width;
+
+            for (int i =0; i< myBoard.Size; i++)
+            {
+
+                for (int j =0; j <myBoard.Size; j++)
+                {
+                    btnGrid[i, j] = new Button();
+
+                    btnGrid[i, j].Height = buttonSize;
+
+                    btnGrid[i, j].Width = buttonSize;
+
+
+                    btnGrid[i, j].Click += Grid_Button_Click;
+
+                    Panel1.Controls.Add(btnGrid[i, j]);
+                    btnGrid[i, j].Location = new Point(i * buttonSize, j * buttonSize);
+                    btnGrid[i, j].Text = i + "|" + j;
+                    btnGrid[i, j].Tag = new Point(i, j);
+                }
+            }
+        }
+
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            //Get th row and column number.
+
+            Button clickedButton = (Button)sender;
+            Point location = (Point)clickedButton.Tag;
+
+            int x = location.X;
+            int y = location.Y;
+            Cell currentCell = myBoard.theGrid[x, y];
+
+            // Determine the next legal move
+
+            myBoard.MarkNextLegalMoves(currentCell, "Knight");
+
+            // update the text in each button.
+
+            for ( int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    btnGrid[i, j].Text = "";
+                    if (myBoard.theGrid[i, j].LegalNextMove == true)
+                    {
+                        btnGrid[i, j].Text = "Legal";
+                    }
+                    else if (myBoard.theGrid[i, j].CurrentlyOccupied == true)
+                    {
+                        btnGrid[i, j].Text = "Knight";
+                    }
+
+                    
+                }
+            }
+             
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
